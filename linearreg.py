9f5854683131dc989cdb5ae8b8b82815e5f1e9fa
Aaron Lin <aaron.tlin@gmail.com>
2020-04-21 15:18:31 -0400

    added psuedocode. started with linear regression so i can test get requests on something i know will work, then moving onto logistic

---
+++ linearreg.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from datetime import datetime as dt
+from datetime import date
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import requests
+
+# Import dataset
+    # df = get request 
+    #y = df[]
+    #x = df[]
+#split dataset
+    
+#set up and train regression
+    #regression = LinearRegression()
+    #regression.fit(x values, y values)
+
+    #y_pred = regression.predict(x)
